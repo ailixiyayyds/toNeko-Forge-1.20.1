35dc9e68b5091f6122af4240983a46437830d7a4
anno <xincpbehp@gmail.com>
2026-07-19 00:31:13 +0800

    fix(forge): remove invalid player shadows

--- common/src/main/java/org/cneko/toneko/common/mod/mixin/PlayerEntityMixin.java
+++ common/src/main/java/org/cneko/toneko/common/mod/mixin/PlayerEntityMixin.java
@@ -32,7 +32,6 @@ import org.cneko.toneko.common.mod.util.EntityUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -43,10 +42,6 @@ import java.util.*;
 
 @Mixin(Player.class)
 public abstract class PlayerEntityMixin implements INeko, PlayerLeashAccess, SlowTickable {
-
-    @Shadow private boolean reducedDebugInfo;
-
-    @Shadow public abstract void remove(Entity.RemovalReason reason);
 
     // ------- 需要死亡不重置 --------
     @Unique
